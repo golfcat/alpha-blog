61b68208ae2854f72a0e3129fff9e5b47273a273
Rails Udemy Course <stellab2004@gmail.com>
2016-06-01 13:10:31 +0000

    Create new template, action and route

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160530175640) do
+ActiveRecord::Schema.define(version: 20160530180136) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
